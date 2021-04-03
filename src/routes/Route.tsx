c28983ad102b3aa2e04341f78805aad3d9d47fc3
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-04-03 14:54:26 -0300

    :wrench: feat: setup private routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/Auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+export const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => (isPrivate === !!user ? (
+        <Component />
+      ) : (
+        <Redirect
+          to={{
+            pathname: isPrivate ? '/' : '/home',
+            state: { from: location },
+          }}
+        />
+      ))}
+    />
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from './Route';
+
+import { Home } from '../pages/Home';
 import { SignIn } from '../pages/SignIn';
 
 export function Routes(): JSX.Element {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+
+      <Route path="/home" exact component={Home} isPrivate />
     </Switch>
   );
 }
