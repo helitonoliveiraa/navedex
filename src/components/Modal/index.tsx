a588f18e0adbf6d8aa610847aca12b57c80dbce0
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-04-04 15:24:59 -0300

    :sparkles: feat: create card and modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,54 @@
+import { rgba } from 'polished';
+import { ReactNode, useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+
+type ModalProps = {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  children: ReactNode;
+};
+
+export function Modal({
+  isOpen,
+  setIsOpen,
+  children,
+}: ModalProps): JSX.Element {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(() => {
+    if (modalStatus !== isOpen) {
+      setModalStatus(isOpen);
+    }
+  }, [isOpen, modalStatus]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '0',
+          width: '100%',
+          maxWidth: '1000px',
+          border: 'none',
+          padding: '0',
+        },
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
